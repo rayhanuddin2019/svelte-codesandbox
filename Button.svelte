b44638d4a81a1fcafec3eba25f7aff60a28769d8
Rayhan <47168831+rayhanuddin2019@users.noreply.github.com>
2021-02-06 12:56:49 +0600

    Initial commit

---
+++ Button.svelte
@@ -0,0 +1,28 @@
+<script>
+	let count = 0;
+	let count2 = 0;
+	function handleClick() {
+	  count += 1;
+	}
+	function handleClick2() {
+	  count2 += 2;
+	}
+</script>
+
+<style>
+	button {
+	  background: #ff3e00;
+	  color: white;
+	  border: none;
+	  padding: 8px 12px;
+	  border-radius: 2px;
+	}
+</style>
+
+<button on:click={handleClick}>
+  Clicked {count} {count === 1 ? 'time' : 'times'}
+</button>
+<hr/>
+<button on:click={handleClick2}>
+  Double Clicked {count2} {count2 === 1 ? 'time' : 'times'}
+</button>
